bddb53f5ca6448e34699455948e4754b1a022133
RD Magboo <rdmagboo@gmail.com>
2013-10-06 07:08:45 +0800

    added new pages

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,14 @@
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+});
 
+var $btn = $("#menu"),
+    $change = $btn.children('div');
+
+$btn.on('click', function(e) {
+  e.preventDefault();
+  $change.toggleClass('change');
+});
