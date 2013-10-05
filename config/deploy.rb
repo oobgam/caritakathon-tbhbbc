051865a365049e13d7edf27b14383330dd437ce7
RD Magboo <rdmagboo@gmail.com>
2013-10-05 17:26:10 +0800

    changed the port

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ server "tbhbbc", :web, :app, :db, primary: true
 set :deploy_to, "/home/#{user}/apps/#{application}"
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
-ssh_options[:port] = SSHPort
+ssh_options[:port] = 22
 
 
 namespace :deploy do
